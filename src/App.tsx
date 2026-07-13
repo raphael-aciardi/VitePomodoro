47a3947b7614ce7f040590985e0974d8e33fcab4
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-07-13 12:54:45 -0300

    feat: criar componente de rodapé

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { DefaultInput } from './components/DefaultInput';
 import Cycles from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -43,6 +44,9 @@ export function App() {
             <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
