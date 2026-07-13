6cc1f0e9682d8cea3a764a1cba49158e24405281
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-07-13 12:54:40 -0300

    feat: adicionar componente de layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { TimerIcon } from 'lucide-react';
-import './assets/styles/global.css';
-import './assets/styles/theme.css';
+import './styles/theme.css';
+import './styles/global.css';
+import { Container } from './components/Container';
 import { Heading } from './components/Heading';
 
 export function App() {
   return (
-    <Heading>
-      Ola
-      <button>
-        <TimerIcon />
-      </button>
-    </Heading>
+    <>
+      <Container>
+        <Heading>Logo</Heading>
+        <section>Logo</section>
+      </Container>
+      <Container>
+        <Heading>Testando meu componete heading</Heading>
+      </Container>
+    </>
   );
 }
