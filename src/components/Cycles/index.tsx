7c3c62e0bc98478d4d5fe8e717fd31a50119c226
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-07-13 12:54:44 -0300

    feat: criar layout dos ciclos

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -2,11 +2,29 @@ import styles from './styles.module.css';
 
 export default function Cycles() {
   return (
-    <div className={styles.cicles}>
+    <div className={styles.cycles}>
       <span>Ciclos:</span>
 
       <div className={styles['cycle-dots']}>
         <span className={`${styles['cycle-dot']} ${styles.workTime}`}></span>
+        <span
+          className={`${styles['cycle-dot']} ${styles.shortBreakTime}`}
+        ></span>
+        <span className={`${styles['cycle-dot']} ${styles.workTime}`}></span>
+        <span
+          className={`${styles['cycle-dot']} ${styles.shortBreakTime}`}
+        ></span>
+        <span className={`${styles['cycle-dot']} ${styles.workTime}`}></span>
+        <span
+          className={`${styles['cycle-dot']} ${styles.shortBreakTime}`}
+        ></span>
+        <span className={`${styles['cycle-dot']} ${styles.workTime}`}></span>
+        <span
+          className={`${styles['cycle-dot']} ${styles.shortBreakTime}`}
+        ></span>
+        <span
+          className={`${styles['cycle-dot']} ${styles.longBreakTime}`}
+        ></span>
       </div>
     </div>
   );
