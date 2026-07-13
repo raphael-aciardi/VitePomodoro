57f19f75859d49c60b4ca3e61b65df2309fdedc5
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-07-13 12:54:46 -0300

    feat: atualizar tema escuro e claro

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,26 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
+
+type AvailaableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailaableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
       <a
@@ -33,6 +52,7 @@ export function Menu() {
         href='#'
         aria-label='Mudar Tema'
         title='Mudar Tema'
+        onClick={handleThemeChange}
       >
         <SunIcon />
       </a>
