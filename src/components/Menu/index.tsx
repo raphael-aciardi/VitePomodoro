8757d64dbd73e9cfbd5537680723f36fc490e62f
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2026-07-13 12:54:46 -0300

    feat: adicionar aria-label e title ao menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,16 +4,36 @@ import styles from './styles.module.css';
 export function Menu() {
   return (
     <nav className={styles.menu}>
-      <a className={styles['menu-link']} href='#'>
+      <a
+        className={styles['menu-link']}
+        href='#'
+        aria-label='Ir para a home'
+        title='Ir para a home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles['menu-link']} href='#'>
+      <a
+        className={styles['menu-link']}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles['menu-link']} href='#'>
+      <a
+        className={styles['menu-link']}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles['menu-link']} href='#'>
+      <a
+        className={styles['menu-link']}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+      >
         <SunIcon />
       </a>
     </nav>
